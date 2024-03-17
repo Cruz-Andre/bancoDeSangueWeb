0ec01b91d99fef3b2776197c2bd378389754482b
Cruz-Andre <cruzandrecal@gmail.com>
2024-03-17 19:12:51 -0300

    destaca seleção na tabela

--- js/manutencaoHosp.js
+++ js/manutencaoHosp.js
@@ -1,6 +1,23 @@
+var linhas = document.querySelectorAll("#tabelaHospitais tr");
+
+linhas.forEach(function (linha) {
+    linha.addEventListener("click", function () {
+        // Remover a classe de todas as linhas
+        linhas.forEach(function (outraLinha) {
+            outraLinha.classList.remove("selecionado");
+        });
+
+        // Adicionar a classe apenas à linha clicada
+        linha.classList.add("selecionado");
+    });
+});
+
+
+
+
 var selecionaLinhaNaTabela = document.querySelector("#tabelaHospitais")
 
-selecionaLinhaNaTabela.addEventListener("click", function(clique) {
+selecionaLinhaNaTabela.addEventListener("click", function (clique) {
     console.log(clique.target);
     var alvoDoClique = clique.target;
     var paiDoAlvo = alvoDoClique.parentNode;
@@ -8,3 +25,5 @@ selecionaLinhaNaTabela.addEventListener("click", function(clique) {
     console.log(paiDoAlvo.cells);
     console.log(paiDoAlvo.cells[0].innerText);
 })
+
+
