6a00137e740bc1bbf628ddbf8b4ae814c3a87e11
Cruz-Andre <cruzandrecal@gmail.com>
2024-03-20 11:40:28 -0300

    Filtro funcional

---
+++ js/filtraHospital.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let inputFiltro = document.getElementById("filtrarTabela");
+    
+    inputFiltro.addEventListener("input", function() {
+        let filtro = inputFiltro.value.toLowerCase();
+        let hospitais = document.querySelectorAll(".hospital");
+        
+        hospitais.forEach(function(hospital) {
+            let nomeHospital = hospital.querySelector(".infoNome").textContent.toLowerCase();
+            
+            if (nomeHospital.includes(filtro)) {
+                hospital.style.display = "table-row";
+            } else {
+                hospital.style.display = "none";
+            }
+        });
+    });
+});
